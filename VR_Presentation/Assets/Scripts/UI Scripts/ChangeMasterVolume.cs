2973cceb3e354393192b1a589efb760bc025ad17
MasterJetson <greeff.claude@gmail.com>
2017-07-24 23:32:04 +0200

    User Interface Asset Files

---
+++ VR_Presentation/Assets/Scripts/UI Scripts/ChangeMasterVolume.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeMasterVolume : MonoBehaviour {
+
+    public Slider sliderMaster;
+    public Slider sliderMusic;
+    public Slider sliderSoundEffects;
+    public float previous = 100;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (sliderMaster.value != previous)
+        {
+            sliderMusic.value = sliderMaster.value;
+            sliderSoundEffects.value = sliderMaster.value;
+            previous = sliderMaster.value;
+        } 
+    }
+}
---
+++ VR_Presentation/Assets/Scripts/UI Scripts/ClickAction.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Threading;
+
+[RequireComponent(typeof(AudioSource))]
+public class ClickAction : MonoBehaviour, IPointerClickHandler
+{
+    public Image currentImage;
+    public AudioSource audio;
+    //public Animation animation;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {         
+        currentImage = GetComponent<Image>();        
+        
+        if(currentImage.sprite.name == "Office")
+        {
+            audio.Play();
+            Thread.Sleep(2000);
+            //animation.Play();            
+            LoadByIndex(1);
+        }
+        else
+        {
+            audio.Play();
+            Thread.Sleep(2000);
+            //animation.Play();            
+            LoadByIndex(2);
+        }
+    }
+     
+
+    public void LoadByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
